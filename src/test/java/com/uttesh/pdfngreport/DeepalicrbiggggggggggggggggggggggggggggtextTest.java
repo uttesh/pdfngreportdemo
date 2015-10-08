899ac0f69e232ab8de2be1a62416698753b92668
androkumar04 <androkumar04@gmail.com>
2015-10-08 23:50:10 +0530

    updated with 2.1.0

---
+++ src/test/java/com/uttesh/pdfngreport/DeepalicrbiggggggggggggggggggggggggggggtextTest.java
@@ -0,0 +1,22 @@
+package com.uttesh.pdfngreport;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DeepalicrbiggggggggggggggggggggggggggggtextTest {
+
+    @Test
+    public void testQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQA(){
+        Assert.assertEquals("A", "B");
+    }
+
+    @Test
+    public void testB() {
+        Assert.assertEquals("B", "B");
+    }
+
+    @Test
+    public void testC() {
+        Assert.assertEquals("C", "c");
+    }
+}
